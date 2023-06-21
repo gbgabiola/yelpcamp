f6c14ae05496d83b830ee65c12a1ed1a67790471
Genesis Gabiola <gbgabiola@gmail.com>
2023-06-21 20:12:21 +0800

    Add campground model

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
-const app = express();
 const PORT = 3000;
+const Campground = require('./models/campground');
+
+mongoose
+  .connect('mongodb://localhost:27017/yelpCamp')
+  .then(() => console.log('Database connected!'))
+  .catch(err => console.log(err));
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/new-campground', async (req, res) => {
+  const camp = new Campground({
+    title: 'Backyard Campsite',
+    location: 'My Backyard',
+    price: 12.99,
+    description: 'Near and cheap camping!',
+  });
+
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
