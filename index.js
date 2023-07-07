e59354c83ca0435cb0b1f0cb3c4a96e5b5390054
Genesis Gabiola <gbgabiola@gmail.com>
2023-07-07 11:07:33 +0800

    Add a route for campground reviews

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const PORT = 3000;
 const { campgroundSchema } = require('./schemas');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 
@@ -49,10 +50,8 @@ app.post(
   '/campgrounds',
   validateCampground,
   catchAsync(async (req, res) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-
     const campground = new Campground(req.body.campground);
-    campground.save();
+    await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -93,6 +92,18 @@ app.delete(
   catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(req.params.id);
     res.redirect('/campgrounds');
+  })
+);
+
+app.post(
+  '/campgrounds/:id/reviews',
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
