98022415fbc4f1cc1f98e4d3ddc5db4087481fbc
Genesis Gabiola <gbgabiola@gmail.com>
2023-07-25 23:46:32 +0800

    Update campground permissions and move middlewares

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { reviewSchema } = require('../schemas');
+const { validateReview } = require('../middleware');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const message = error.details.map(detail => detail.message).join(',');
-    throw new ExpressError(message, 400);
-  } else {
-    next();
-  }
-};
 
 router.post(
   '/',
