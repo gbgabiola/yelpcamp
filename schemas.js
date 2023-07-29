a95602d3db8fcf41283e1f3d87e381b177036699
Genesis Gabiola <gbgabiola@gmail.com>
2023-07-29 21:59:32 +0800

    Store uploaded images links in MongoDB

--- schemas.js
+++ schemas.js
@@ -4,7 +4,6 @@ module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
     location: Joi.string().required(),
-    image: Joi.string().required(),
     price: Joi.number().min(0).required(),
     description: Joi.string().required(),
   }).required(),
