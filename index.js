1041546abe8f968612e50551edda31a59a147f1d
Genesis Gabiola <gbgabiola@gmail.com>
2023-09-25 23:39:25 +0800

    Update session store with Mongo

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ const mongoSanitize = require('express-mongo-sanitize');
 const passport = require('passport');
 const path = require('path');
 const session = require('express-session');
-const PORT = 3000;
+const MongoStore = require('connect-mongo');
 const User = require('./models/user');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 const ExpressError = require('./utils/ExpressError');
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelpCamp';
+
 mongoose
-  .connect('mongodb://localhost:27017/yelpCamp')
+  .connect(dbUrl)
   .then(() => console.log('Database connected!'))
   .catch(err => console.log(err));
 
@@ -34,11 +36,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(mongoSanitize());
+app.use(mongoSanitize({ replaceWith: '_' }));
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret,
+  },
+});
+
+store.on('error', function (e) {
+  console.log('Session Store Error', e);
+});
 
 const sessionConfig = {
+  store,
   name: 'session',
-  secret: 'thisshouldbeabettersecret!',
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -126,4 +142,5 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
