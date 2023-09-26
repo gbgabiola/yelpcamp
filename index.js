c8ab865a60cf219fd771fe4c3c4fd74f795147b7
Genesis Gabiola <gbgabiola@gmail.com>
2023-09-26 11:22:02 +0800

    Add "/" to cloudinary imgSrc in contentSecurityPolicy

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.use(
         "'self'",
         'blob:',
         'data:',
-        `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}`,
+        `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
         'https://images.unsplash.com',
       ],
       fontSrc: ["'self'", ...fontSrcUrls],
