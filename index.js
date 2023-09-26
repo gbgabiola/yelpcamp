2390475c78c69355f3fe9fa25285db51c69cd227
Genesis Gabiola <gbgabiola@gmail.com>
2023-09-26 10:22:44 +0800

    Add npm script and some missing content security policy directives

--- index.js
+++ index.js
@@ -69,25 +69,24 @@ app.use(flash());
 app.use(helmet());
 
 const scriptSrcUrls = [
-  'https://api.tiles.mapbox.com/',
-  'https://api.mapbox.com/',
-  'https://kit.fontawesome.com/',
-  'https://cdnjs.cloudflare.com/',
+  'https://api.tiles.mapbox.com',
+  'https://api.mapbox.com',
+  'https://kit.fontawesome.com',
+  'https://cdnjs.cloudflare.com',
   'https://cdn.jsdelivr.net',
 ];
 const styleSrcUrls = [
-  'https://kit-free.fontawesome.com/',
-  'https://api.mapbox.com/',
-  'https://api.tiles.mapbox.com/',
-  'https://fonts.googleapis.com/',
-  'https://use.fontawesome.com/',
+  'https://kit-free.fontawesome.com',
+  'https://api.mapbox.com',
+  'https://api.tiles.mapbox.com',
+  'https://fonts.googleapis.com',
+  'https://use.fontawesome.com',
   'https://cdn.jsdelivr.net',
 ];
 const connectSrcUrls = [
-  'https://api.mapbox.com/',
-  'https://a.tiles.mapbox.com/',
-  'https://b.tiles.mapbox.com/',
-  'https://events.mapbox.com/',
+  'https://api.mapbox.com',
+  'https://*.tiles.mapbox.com',
+  'https://events.mapbox.com',
 ];
 const fontSrcUrls = [];
 app.use(
@@ -98,13 +97,14 @@ app.use(
       scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
       styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
       workerSrc: ["'self'", 'blob:'],
+      childSrc: ['blob:'],
       objectSrc: [],
       imgSrc: [
         "'self'",
         'blob:',
         'data:',
-        `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
-        'https://images.unsplash.com/',
+        `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}`,
+        'https://images.unsplash.com',
       ],
       fontSrc: ["'self'", ...fontSrcUrls],
     },
